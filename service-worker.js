767ba3310e0a26af894273faf59cac5e5e351d35
LVeraPizza <veinticinco95@hotmail.com>
2025-01-04 19:15:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,8 @@ self.addEventListener('install', (event) => {
         'https://lverapizza.github.io/App/css/styles.css',
         'https://lverapizza.github.io/App/script.js',
         'https://lverapizza.github.io/App/splash.js',
-        'https://lverapizza.github.io/App/service-worker.js',
         'https://lverapizza.github.io/App/logo_vp.png',
         'https://lverapizza.github.io/App/img/eventos/logo_año_nuevo.png',
-        'https://lverapizza.github.io/App/firebase-messaging-sw.js',
 
         // Agrega otros archivos que necesites en el caché
       ]);
